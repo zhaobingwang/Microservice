97dfc8d201fbce47e7c9037d78070e737560bc92
No.8 <zhaobingwang@foxmail.com>
2018-03-07 23:30:47 +0800

    用户服务EF Core模型设计

---
+++ User.API/User.API/Models/UserProperty.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace User.API.Models
+{
+    public class UserProperty
+    {
+        public int UserId { get; set; }
+        public string Key { get; set; }
+        public string Text { get; set; }
+        public string Value { get; set; }
+    }
+}
---
+++ User.API/User.API/Models/UserTag.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace User.API.Models
+{
+    public class UserTag
+    {
+        public int UserId { get; set; }
+        public string Tag { get; set; }
+
+        /// <summary>
+        /// 创建时间
+        /// </summary>
+        public DateTime CreateTime { get; set; }
+    }
+}
