5931b8af63ab3f440deffa9b04df7409f6744922
no8 <zhaobingwang@foxmail.com>
2018-03-22 23:34:00 +0800

    数据库迁移

---
+++ User.API/User.API/Migrations/20180322150640_AddUserEntites.cs
@@ -0,0 +1,108 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace User.API.Migrations
+{
+    public partial class AddUserEntites : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "UserBPFiles",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("MySQL:AutoIncrement", true),
+                    CreateTime = table.Column<DateTime>(nullable: false),
+                    FileName = table.Column<string>(nullable: true),
+                    FormatFilePath = table.Column<string>(nullable: true),
+                    OriginFilePath = table.Column<string>(nullable: true),
+                    UserId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserBPFiles", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("MySQL:AutoIncrement", true),
+                    Address = table.Column<string>(nullable: true),
+                    Avator = table.Column<string>(nullable: true),
+                    City = table.Column<string>(nullable: true),
+                    CityId = table.Column<int>(nullable: false),
+                    Company = table.Column<string>(nullable: true),
+                    Email = table.Column<string>(nullable: true),
+                    Name = table.Column<string>(nullable: true),
+                    NameCard = table.Column<string>(nullable: true),
+                    Phone = table.Column<string>(nullable: true),
+                    Province = table.Column<string>(nullable: true),
+                    ProvinceId = table.Column<int>(nullable: false),
+                    Tel = table.Column<string>(nullable: true),
+                    Title = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UserTags",
+                columns: table => new
+                {
+                    UserId = table.Column<int>(nullable: false),
+                    Tag = table.Column<string>(maxLength: 100, nullable: false),
+                    CreateTime = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserTags", x => new { x.UserId, x.Tag });
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UserProperities",
+                columns: table => new
+                {
+                    Key = table.Column<string>(maxLength: 100, nullable: false),
+                    UserId = table.Column<int>(nullable: false),
+                    Value = table.Column<string>(maxLength: 100, nullable: false),
+                    AppUserId = table.Column<int>(nullable: true),
+                    Text = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserProperities", x => new { x.Key, x.UserId, x.Value });
+                    table.ForeignKey(
+                        name: "FK_UserProperities_Users_AppUserId",
+                        column: x => x.AppUserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserProperities_AppUserId",
+                table: "UserProperities",
+                column: "AppUserId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "UserBPFiles");
+
+            migrationBuilder.DropTable(
+                name: "UserProperities");
+
+            migrationBuilder.DropTable(
+                name: "UserTags");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
