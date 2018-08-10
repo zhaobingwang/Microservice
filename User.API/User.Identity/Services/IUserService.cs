6746f47352c2f232c2abd6d477a6d7e8af86aed9
Route8 <zhaobingwang@foxmail.com>
2018-08-10 22:08:55 +0800

    identityserver4自定义validator

---
+++ User.API/User.Identity/Services/IUserService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace User.Identity.Services
+{
+    public interface IUserService
+    {
+        /// <summary>
+        /// 检查手机号是否已注册，若有注册则注册一个用户
+        /// </summary>
+        /// <param name="phone">手机号</param>
+        /// <returns>用户Id</returns>
+        int CheckOrCreate(string phone);
+    }
+}
---
+++ User.API/User.Identity/Services/TestAuthCodeService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace User.Identity.Services
+{
+    public class TestAuthCodeService : IAuthCodeService
+    {
+        /// <summary>
+        /// 测试环境使用
+        /// </summary>
+        /// <param name="phone"></param>
+        /// <param name="authCode"></param>
+        /// <returns></returns>
+        public bool Validate(string phone, string authCode)
+        {
+            return true;
+        }
+    }
+}
